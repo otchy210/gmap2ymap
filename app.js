d59430c5d212b9bc3879dfdd930e297bbdca9ef1
Otchy <id+github@indeed.com>
2019-05-30 23:56:49 +0900

    Get lat & long from Google Map shorten url

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
+const parseUrl = require('url').parse;
+
+const PAGE_TIMEOUT = 5000;
+const CHECK_REDIRECT_TIMNEOUT = 10000;
+const CHECK_REDIRECT_INTERVAL = 100;
+const LAT_LONG_URL_REGEX = /.+\/@([\d\.]+),([\d\.]+),[\d]+z\/.+/;
 
 const app = express();
 
@@ -8,17 +14,48 @@ app.disable('x-powered-by');
 
 app.get('/', (req, res) => {
     if (req.hostname !== 'localhost' && req.get('X-Forwarded-Proto') === 'http') {
-        res.status(403).end();
+        res.status(400).send('Require HTTPS');
+        return;
+    }
+    const url = req.query.url;
+    if (!url) {
+        res.status(400).send('url parameter is required');
+        return;
+    }
+    const parsedUrl = parseUrl(url);
+    const {protocol, host, pathname} = parsedUrl;
+    if (!protocol || !host || !pathname) {
+        res.status(400).send('invalid url format');
         return;
     }
     (async () => {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
-        await page.goto('https://www.otchy.net', {waitUntil: 'domcontentloaded'});
-        const html = await page.$eval('html', el => {
-            return el.innerHTML;
-        });
-        res.send(html);
+        const pageRes = await page
+            .goto(url, {waitUntil: 'domcontentloaded', timeout: PAGE_TIMEOUT})
+            .catch(e => console.error(e));
+        if (!pageRes) {
+            res.status(500).send(`Couldn't access ${url} in ${PAGE_TIMEOUT} msec`);
+            return;
+        }
+        const timeStarted = (new Date()).getTime();
+        const checkRedirect = () => {
+            const pageUrl = page.url();
+            const results = LAT_LONG_URL_REGEX.exec(pageUrl);
+            if (results) {
+                const lat = results[1];
+                const long = results[2];
+                res.set('Content-Type', 'text/plain').send(`lat = ${lat}, long = ${long}\n`);
+                return;
+            }
+            const timeSpent = (new Date()).getTime() - timeStarted;
+            if (timeSpent >= CHECK_REDIRECT_TIMNEOUT) {
+                res.status(500).send(`Couldn't get lat and long from ${pageUrl} in ${CHECK_REDIRECT_INTERVAL} msec`);
+                return;
+            }
+            setTimeout(checkRedirect, CHECK_REDIRECT_INTERVAL);
+        };
+        checkRedirect();
     })();
 });
 
